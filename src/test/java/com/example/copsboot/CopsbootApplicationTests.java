45395a054df231cad05df1662f313ff7cb56e172
diana <zS22017039@estudiantes.uv.mx>
2025-04-27 18:13:06 -0600

    test updated

--- src/test/java/com/example/copsboot/CopsbootApplicationTests.java
+++ src/test/java/com/example/copsboot/CopsbootApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.copsboot;
 
+import com.example.copsboot.infraestructure.SpringProfiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(SpringProfiles.INTEGRATION_TEST)
 class CopsbootApplicationTests {
 
 	@Test
